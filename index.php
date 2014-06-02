8f372c6b4e4b6bf1ef498a4bd65684ab309a295a
Mellgood <cacento@hotmail.it>
2014-06-03 01:58:54 +0200

    ripartiamo da 0 aggiunta classe EPersona aggiunto package Utility aggiunta libreria di gestione del database Fdb.php aggiunta cartella config e file di configurazione di base aggiunto creaDB.sql comprensivo di dump utenti Mellgood e Splashzzz. Mellgood è paziente, splashzzzz no. aggiunta FPersona, strato foundation dell'entità EPersona. IL LOGIN FUNZIONA , si puo testare con una semplice form a cazzo

--- index.php
+++ index.php
@@ -3,11 +3,36 @@ File creato da Carlo Centofanti
 -->
 
 <?php
+require_once 'lib/smarty/Smarty.class.php';
 require_once 'config/autoload.inc.php';
 require_once 'config/config.inc.php';
 
+
 $CLogin=new CLogin();
-if ($CLogin->autentica('Mellgood', '0000'))
+$smarty= new Smarty();
+
+global $config;
+$smarty->template_dir = $config['smarty']['template_dir'];
+$smarty->compile_dir = $config['smarty']['compile_dir'];
+$smarty->config_dir = $config['smarty']['config_dir'];
+$smarty->cache_dir = $config['smarty']['cache_dir'];
+$smarty->caching = false;
+
+
+
+
+$smarty->display('login.tpl');
+
+//parte che va nella view
+if (isset($_REQUEST['username']))
+    $username = $_REQUEST['username'];
+else $username="vuoto";
+
+if (isset($_REQUEST['password']))
+    $password = $_REQUEST['password'];
+else$password="vuoto";
+
+if ($CLogin->autentica($username, $password))
         echo "autenticato";
 else echo "non autenticato :(";
         
