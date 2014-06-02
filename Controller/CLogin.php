058fd5dcc3497d1c57d0014c253e508ea6b8d684
Mellgood <cacento@hotmail.it>
2014-06-02 16:37:46 +0200

    ripartiamo da 0 aggiunta classe EPersona aggiunto package Utility aggiunta libreria di gestione del database Fdb.php aggiunta cartella config e file di configurazione di base aggiunto creaDB.sql comprensivo di dump utenti Mellgood e Splashzzz. Mellgood è paziente, splashzzzz no. aggiunta FPersona, strato foundation dell'entità EPersona.

---
+++ Controller/CLogin.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * File creato da Carlo Centofanti
+ */
+
+/**
+ * Description of CLogin
+ *
+ * @access public
+ * @package Controller
+ * @author Carlo
+ */
+class CLogin {
+    //put your code here
+}
+
+?>
