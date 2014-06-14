4aeaf5039dc6a5b3001e90fd1b49e338e5c31b28
Mellgood <cacento@hotmail.it>
2014-06-14 13:08:33 +0200

    adesso il login funziona, viene pushato in un template vuoto solo quando l'utente non è loggato. il controllo se è loggato viene fatto su index.php. va spostato in CHome quando sarà pronta

--- index.php
+++ index.php
@@ -17,9 +17,32 @@ $smarty->config_dir = $config['smarty']['config_dir'];
 $smarty->cache_dir = $config['smarty']['cache_dir'];
 $smarty->caching = false;
 
+//sta parte dovrebbe pushare nella home il login fetchato da VLogin... ora va ma non 
+//prende i bottoni... boh
+$VLogin= USingleton::getInstance('VLogin');
+$loggato=$CLogin->smista();
+
+if(!$loggato){
+    $contenuto=$VLogin->processaTemplate();
+    
+    $smarty->assign('contenuto', $contenuto);
+    $smarty->assign ('messaggio', "non autenticato :(");
+    $smarty->display('templateVuoto.tpl');
+}
 
 
 
+/*
+$loggato=$CLogin->smista();
+if(!$loggato){
+    $smarty->assign ('messaggio', "non autenticato :(");
+    
+}else $smarty->assign ('messaggio', "loggato!! :)");
+*/
+
+
+
+/*
     
 $session=USingleton::getInstance('USession');
 if(!$session->leggi_valore('username')){
@@ -37,8 +60,8 @@ if(!$session->leggi_valore('username')){
             $smarty->assign ( 'messaggio',"autenticato tramite login");
     else$smarty->assign ('messaggio', "non autenticato :("); //ci va l'errore recuperato da clogin mediante get
 }else $smarty->assign ('messaggio','autenticato tramite sessione :)');    
-    
-$smarty->display('login.tpl');
+    */
+//$smarty->display('login.tpl');
 
 ?>
 
